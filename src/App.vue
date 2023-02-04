c04e770ffafe42b7723d83d37b2872c4b5c086a1
ryfazrin <riansera515@gmail.com>
2023-02-04 11:27:15 +0700

    created: show all notes

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,33 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+import { getAllNotes } from './utils/local-data'
+import { showFormattedDate } from './utils'
 
+const allNotes = ref(getAllNotes()) 
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="app-container">
+    <header>
+      <h1><a href="/">Aplikasi Catatan</a></h1>
+      <nav class="navigation">
+        <ul>
+          <li><a href="/archives">Arsip</a></li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <section class="notes-list">
+        <article v-for="note in allNotes" class="note-item">
+          <h3 class="note-item__title">
+            {{ note.title }}
+          </h3>
+          <p class="note-item__createdAt">{{ showFormattedDate(note.createdAt) }}</p>
+          <p class="note-item__body">{{ note.body }}</p>
+        </article>
+      </section>
+    </main>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
