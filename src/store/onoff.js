d2fcc6ff4b4ce7af9e1dbc1bc9d8c04062a86a04
ryfazrin <riansera515@gmail.com>
2023-02-07 17:22:56 +0700

    how use global state ref

---
+++ src/store/onoff.js
@@ -0,0 +1,37 @@
+import { computed, reactive, ref, toRefs } from "vue";
+
+// const state = reactive({
+//   onoff: 1
+// })
+
+// export default function useOnoff() {
+//   const onoff = state.onoff
+
+//   function increment() {
+//     state.onoff++
+//   }
+
+//   return {
+//     ...toRefs(onoff),
+//     onoff,
+//     increment
+//   }
+// }
+
+const state = ref({
+  onoff: 0,
+})
+
+export default function useOnoff() {
+  const onoff = computed(() => state.value.onoff)
+  function increment() {
+    state.value.onoff++
+    console.log(state.value.onoff)
+  }
+
+  return {
+    ...toRefs(onoff),
+    onoff,
+    increment
+  }
+}
